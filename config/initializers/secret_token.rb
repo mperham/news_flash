1ac869bdea71501664ab3ae0507c9566a1582891
Mike Perham <mperham@gmail.com>
2011-11-09 19:10:16 -0800

    Rails 3.1.1 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsFlash::Application.config.secret_token = 'd4bf3a878b5cd86f67616292b26784f7ad3095a5b948f44536d0224147c58557a5f352a947e961e92ccd00d9c443b1c2e731a40044c6273b1aa83cca50cc1d03'
